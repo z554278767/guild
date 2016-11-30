c10bc497da63da04e8c85ea4209a98da16dfc260
Yuan <953608343@qq.com>
2016-11-30 19:11:53 +0800

    //登录

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:api');
+//登录
+Route::any('/login','LoginController@login');
